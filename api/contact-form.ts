856d33a80fa516632861e0d4f9a94faacc2d0f7d
Jocsan Rueda <jocsanelyrueda@gmail.com>
2026-05-10 14:51:16 -0600

    fix: rename nombre_formulario column to form in contact-form database schema

--- api/contact-form.ts
+++ api/contact-form.ts
@@ -24,7 +24,7 @@ export default async function handler(
       .from("Form_BOA")
       .insert([
         { 
-          nombre_formulario: "form-contact", 
+          form: "form-contact", 
           data: body 
         }
       ])
